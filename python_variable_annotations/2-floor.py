913d2a9fbf9409884939faa85fc07641b3fbf9d6
Robin-1998 <10616@holbertonstudents.com>
2025-07-28 11:01:14 +0200

    ajout du fichier pour la tache 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Project Holberton
+Task 2. Basic annotations - floor
+"""
+
+
+def floor(n: float) -> float:
+    """
+    Write a type-annotated function add
+    """
+    return (n)
