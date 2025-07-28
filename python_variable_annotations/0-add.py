cdcb85b249654ebc5182ef0d45ff02f839a6a027
Robin-1998 <10616@holbertonstudents.com>
2025-07-28 10:41:30 +0200

    correctif float

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -7,6 +7,6 @@ Task 0 :  Basic annotations - add
 """
 
 
-def add(a: float, b: float) -> str:
+def add(a: float, b: float) -> float:
     """ Write a type-annotated function add """
     return(a + b)
