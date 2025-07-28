e7ee412992b1ce001b138097ed4c5bbd29237c84
Robin-1998 <10616@holbertonstudents.com>
2025-07-28 14:34:18 +0200

    correctif task 7, pour la racine carré

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,11 +3,11 @@
 Project Holberton
 Task 7. Complex types - string and int/float to tuple
 """
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv (k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv (k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Write a type-annotated function to_kv
     """
-    return (k, float(v))
+    return (k, v**2)
