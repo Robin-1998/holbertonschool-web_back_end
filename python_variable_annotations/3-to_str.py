b12c1880f7ba28ea75b5429d8a1c23dface313f3
Robin-1998 <10616@holbertonstudents.com>
2025-07-28 11:10:26 +0200

    ajout du fichier pour la tache 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Project Holberton
+Task 3. Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Write a type-annotated function add
+    """
+    return str(n)
