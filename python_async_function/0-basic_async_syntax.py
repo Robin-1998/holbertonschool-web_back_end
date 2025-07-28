c646b7ac0c1cbb1972bfd81ee701f4d269884b39
Robin-1998 <10616@holbertonstudents.com>
2025-07-28 17:07:10 +0200

    ajout du float sur value

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -3,6 +3,6 @@ import random
 import asyncio
 
 async def wait_random(max_delay = 10) -> float:
-    value = random.uniform(0, max_delay)
+    value:float = random.uniform(0, max_delay)
     await asyncio.sleep(value)
     return (value)
