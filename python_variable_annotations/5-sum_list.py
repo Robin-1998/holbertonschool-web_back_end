31d62a29e8d13ae1b5d8cd1ac5e9d8d736f98d77
Robin-1998 <10616@holbertonstudents.com>
2025-07-28 11:52:00 +0200

    ajout du fichier pour la tache 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from typing import List
+"""
+Project Holberton
+Task 5. Complex types - list of floats
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    return sum(input_list)
