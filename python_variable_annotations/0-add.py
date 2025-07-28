a72539b64de9858b93417546eee986ff53a3d5ef
Robin-1998 <10616@holbertonstudents.com>
2025-07-28 10:39:31 +0200

    correctif fichier tache 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+"""
+Project Holberton
+Task 0 :  Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> str:
+    """ Write a type-annotated function add """
+    return(a + b)
