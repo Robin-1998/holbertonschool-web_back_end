66e442b37a830884faf6bc6cfafda9a1ece61ddc
Robin-1998 <10616@holbertonstudents.com>
2025-07-28 14:35:38 +0200

    correctif pycode

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -6,7 +6,7 @@ Task 7. Complex types - string and int/float to tuple
 from typing import Union, Tuple
 
 
-def to_kv (k: str, v: Union[int, float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Write a type-annotated function to_kv
     """
