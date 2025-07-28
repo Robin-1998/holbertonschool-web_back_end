cdfa470f8bb7da83b511d401000997c3756cf7a8
Robin-1998 <10616@holbertonstudents.com>
2025-07-28 10:45:01 +0200

    re pycode

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """
 Project Holberton
-Task 0 :  Basic annotations - add
+Task 0 : Basic annotations - add
 """
 
 
 def add(a: float, b: float) -> float:
-    """ Write a type-annotated function add """
+    """
+    Write a type-annotated function add
+    """
     return(a + b)
