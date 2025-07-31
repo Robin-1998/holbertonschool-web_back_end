266e9e0007c22916bdb84b2d614d61a3f0b62d4d
Robin-1998 <10616@holbertonstudents.com>
2025-07-31 10:57:43 +0200

    ajout du fichier pour la tache 2

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Project Holberton -
+2. Measure the runtime
+"""
+
+
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Function measure the runtime of the function wait_n
+    n en argument correpond au nombre de fois que wait_n doit être exécutés
+    max_delay comme son nom l'indique est le délais maximum à utiliser 
+    dans wait_n
+    """
+    start_time = time.time()
+    # à l'aide de l'import time on peut retourner le temps en secondes
+    # time.time va retourne l'heure actuelle sous forme à virgules et d'une
+    # manière très précise (jusqu'à la microseconde)
+    asyncio.run(wait_n(n, max_delay))
+    # on exécute wait_n, asyncio.run permet d'exécuter une coroutine
+    # car une fonction async (qui est une coroutine // fonction asynchrone) 
+    # ne peut pas être exécuter comme une fonction
+    # normale. c'est là qu'asyncio.run permet de lancer une coroutine depuis
+    # un code synchrone
+    end_time = time.time()
+    # On calcule le temps écoulé puis on le divise par n pour obtenir
+    # le temps moyen
+    return (end_time - start_time) / n
+
+# Créez une fonction measure_time avec les entiers n et max_delay comme arguments qui mesure le temps d'exécution total pour wait_n(n, max_delay) et renvoie total_time / n. Votre fonction doit renvoyer un nombre à virgule flottante.
+# Utilisez le module time pour mesurer le temps écoulé approximatif.
