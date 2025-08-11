173daef03da32fcd01cf6c8ffd19a2cfe5c3750d
Robin-1998 <10616@holbertonstudents.com>
2025-08-11 10:40:59 +0200

    correctif tache 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,11 +2,12 @@
 """ Simple helper function """
 from typing import List
 
-def index_range (page:int, page_size:int) -> List[int]:
+
+def index_range(page: int, page_size: int) -> List[int]:
+    """
+    function named index_range that takes two
+    integer arguments page and page_size
+    """
     debut = (page - 1) * page_size
     fin = debut + page_size
     return (debut, fin)
-
-# On doit retourner un tuple de taille 3 contenant un idece de début et un indice de fin
-# correspondant à la plage d'indices à renvoyer dans une liste pour ces paramètres
-# de pagniation particuliers
