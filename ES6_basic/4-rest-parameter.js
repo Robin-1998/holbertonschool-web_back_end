aa1aff977bd720717efd1de7abc14e14b76e9209
Robin-1998 <10616@holbertonstudents.com>
2025-08-19 14:20:23 +0200

    ajout du fichier pour la tache 4

---
+++ ES6_basic/4-rest-parameter.js
@@ -0,0 +1,5 @@
+export default function returnHowManyArguments(...argurment) {
+  return argurment.length;
+}
+// les ... permette de regrouper plusieurs arguments en un seul tableau
+// et le fait de faire un length donne le nombre d'argument
