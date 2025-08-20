5cfeaea24518c5ef5b158843b2cb38e6c85e5898
Robin-1998 <10616@holbertonstudents.com>
2025-08-20 13:00:51 +0200

    ajout des fichiers pour la tache 0

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,9 @@
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+}
+}
+
+// constructor permet de créer une nouvelle instance de la class
+// this permet d'initialiser la velur passé au constructeur
+// This permet de récupérer après la variable pour l'utiliser plus tard
