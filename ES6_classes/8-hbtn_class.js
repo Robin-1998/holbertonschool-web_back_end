a25ae4b90d91c0899f2ef3371784848eb3f29c72
Robin-1998 <10616@holbertonstudents.com>
2025-08-21 13:39:23 +0200

    ajout du fichier pour la tache 8

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,17 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
+
+// ToString -> retourne un caractère
+// valueOf -> retourne un nombre
