ef2988ec2c223019a62844a408d0ebd1b55f9ebc
Robin-1998 <10616@holbertonstudents.com>
2025-08-25 11:19:43 +0200

    correctif syntaxe des paramètre

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,10 @@
-export default function getListStudentIds(dax) {
+export default function getListStudentIds(students) {
   if (!Array.isArray(dax)) {
   // si l'argument n'est pas un tableau, la fonction retourne un tableau vide
     return [];
   }
   else {
-    return dax.map((b) => b.id);
+    return students.map((b) => b.id);
     // on parcours chaque élément du tableay dax et retourne un nouveau
     // tableau contenant uniquement la propriété id de chaque élément b
 }
