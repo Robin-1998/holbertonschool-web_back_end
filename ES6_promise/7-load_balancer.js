f22c7114302ce17e073d66696e66e8289dc79767
Robin-1998 <10616@holbertonstudents.com>
2025-08-28 09:42:04 +0200

    ajout commentaire pour déf de race

--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -1,3 +1,5 @@
 export default function loadBalancer(chinaDownload, USDownload) {
   return Promise.race([chinaDownload, USDownload]);
 }
+// La Promise.race()méthode statique prend en entrée un itérable de
+// promesses et renvoie un seul Promise.
