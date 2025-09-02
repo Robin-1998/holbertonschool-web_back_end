084bfad4c58b84d6a31deed30f5fde6b32041aa6
Robin-1998 <10616@holbertonstudents.com>
2025-09-02 07:29:05 +0200

    ajout du fichier pour la tache 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.resume();
+process.stdin.on('data', (data) => {
+  const name = data.trim();
+  console.log(`Your name is: ${name}`);
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
