ceb3300a545f748b209405600bc42659f542829e
Robin-1998 <10616@holbertonstudents.com>
2025-09-02 10:34:44 +0200

    correctif eslint

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,6 +5,6 @@ process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
 });
 
-process.stdin.on('end',() => {
-process.stdout.write('This important software is now closing\n');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
