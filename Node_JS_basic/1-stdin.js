7905cfb54c49211ec1b5d7c18ec47a660a792702
Robin-1998 <10616@holbertonstudents.com>
2025-09-02 09:32:55 +0200

    correctif tache 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,22 +1,16 @@
 console.log('Welcome to Holberton School, what is your name?');
+// display a message
+
 // Active la lecture de l'entrée standard (le clavier)
 process.stdin.resume();
 
-// Écoute l'événement 'readable' → déclenché quand il y a des données disponibles à lire
-process.stdin.on('readable', () => {
-  // Lit les données entrées par l'utilisateur
-  const name = process.stdin.read();
+// Pour convertir automatiquement en chaîne
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', (data) => {
+  const name = data.trim(); // Retire le retour à la ligne automatique
 
-  // Affiche le nom de l'utilisateur tel qu'il a été entré (avec retour à la ligne inclus)
-  process.stdout.write(`Your name is: ${name}`);
+  console.log(`Your name is: ${name}`);
+  console.log('This important software is now closing');
 
-  // Vérifie si le terminal est "interactif" (TTY = terminal textuel)
-  if (process.stdin.isTTY) {
-    // Si oui, quitte immédiatement le programme
-    process.exit();
-  } else {
-    // Sinon, affiche le message de fermeture avant de quitter
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
-  }
+  process.exit();
 });
